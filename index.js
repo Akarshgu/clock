9d2c8f3ad998a5db0c143834dfbdf58a98314c02
Akarshgu <akarshgupta705415>
2022-02-24 20:46:50 +0530

    first

---
+++ index.js
@@ -0,0 +1,48 @@
+// let a=10;
+// var b=20;
+
+// sum();
+// function sum()
+// {
+//     let add= a+b;
+
+//     console.log(add);
+//     subtract();
+//     function subtract(){
+//         let sub =a-b;
+//         console.log(sub);
+//     }
+// }
+
+
+// unary
+// let/var/const
+
+
+// ++a-->pre increment
+//a++-->post increment
+
+
+
+let a=10;
+let count = ++a;
+console.log(count);
+console.log(a);
+
+
+//binary operators is between two operators
+
+// let a=10;
+// let b=20;
+
+// let c= a+b;
+// let c= a-b;
+// let c= a/b;
+// let c= a+b;
+// let c= a+b;
+// let c= a+b;
+let a=12;
+let b =25;
+count= a & b;
+console.log(count);
+
